eb4f9825fb860336198f24914c8ef35b7f7ec3af
Santiago Forero <santiago.forero-y@mail.escuelaing.edu.co>
2023-10-20 19:18:27 -0500

    PARTE 1

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,23 +1,23 @@
 var app = (function () {
 
-    class Point{
-        constructor(x,y){
-            this.x=x;
-            this.y=y;
-        }        
+    class Point {
+        constructor(x, y) {
+            this.x = x;
+            this.y = y;
+        }
     }
-    
+
     var stompClient = null;
 
-    var addPointToCanvas = function (point) {        
+    var addPointToCanvas = function (point) {
         var canvas = document.getElementById("canvas");
         var ctx = canvas.getContext("2d");
         ctx.beginPath();
         ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
         ctx.stroke();
     };
-    
-    
+
+
     var getMousePosition = function (evt) {
         canvas = document.getElementById("canvas");
         var rect = canvas.getBoundingClientRect();
@@ -32,32 +32,37 @@ var app = (function () {
         console.info('Connecting to WS...');
         var socket = new SockJS('/stompendpoint');
         stompClient = Stomp.over(socket);
-        
+
         //subscribe to /topic/TOPICXX when connections succeed
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
-            stompClient.subscribe('/topic/TOPICXX', function (eventbody) {
-                
-                
+            stompClient.subscribe('/topic/newpoint', function (eventbody) {
+                var theObject = JSON.parse(eventbody.body);
+
+                var x = theObject.x;
+                var y = theObject.y;
+
+                alert("Coordenada x: "+x+", Coordenarda y: "+y);
             });
         });
 
     };
-    
-    
+
+
 
     return {
 
         init: function () {
             var can = document.getElementById("canvas");
-            
+
             //websocket connection
             connectAndSubscribe();
         },
 
-        publishPoint: function(px,py){
-            var pt=new Point(px,py);
-            console.info("publishing point at "+pt);
+        publishPoint: function (px, py) {
+            var pt = new Point(px, py);
+            console.info("publishing point at " + pt);
+            stompClient.send("/topic/newpoint", {}, JSON.stringify(pt));
             addPointToCanvas(pt);
 
             //publicar el evento
